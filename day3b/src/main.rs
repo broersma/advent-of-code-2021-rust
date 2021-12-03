9e86f603cbf0342632a5dc89a9b44a0d43bd0dc1
Eric Broersma <git@ericbroersma.nl>
2021-12-03 16:59:09 +0100

    Day 3

---
+++ day3b/src/main.rs
@@ -0,0 +1,60 @@
+use itertools::{Itertools, Either};
+
+pub fn main() {
+    let input_nums: Vec<_> = include_str!("../../day3.txt")
+        .lines()
+        .map(|s| u32::from_str_radix(s, 2).unwrap())
+        .collect();
+
+    let mut nums = input_nums.clone();
+
+    for i in (0..12).rev() {
+        let (zeroes, ones): (Vec<_>, Vec<_>) = nums.iter()
+                                        .partition_map(|r| {
+                                            if r & (1 << i) == 0 {
+                                                Either::Left(r)
+                                            } else {
+                                                Either::Right(r)
+                                            }
+                                        });
+
+        nums = if zeroes.len() > ones.len() {
+            zeroes
+        } else {
+            ones
+        };
+
+        if nums.len() == 1 {
+            break;
+        }
+    }
+
+    let oxy = nums[0];
+
+    let mut nums = input_nums;
+
+    for i in (0..12).rev() {
+        let (zeroes, ones): (Vec<_>, Vec<_>) = nums.iter()
+                                        .partition_map(|r| {
+                                            if r & (1 << i) == 0 {
+                                                Either::Left(r)
+                                            } else {
+                                                Either::Right(r)
+                                            }
+                                        });
+
+        nums = if zeroes.len() <= ones.len() {
+            zeroes
+        } else {
+            ones
+        };
+
+        if nums.len() == 1 {
+            break;
+        }
+    }
+
+    let co2 = nums[0];
+
+    println!("{}", oxy * co2);
+}
