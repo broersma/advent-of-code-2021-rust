6bdc5b3c0e8308362dfd771f0a36dc148b2f08ad
Eric Broersma <git@ericbroersma.nl>
2021-12-03 12:46:58 +0100

    Day 2

---
+++ day2b/src/main.rs
@@ -0,0 +1,24 @@
+pub fn main() {
+    let mut depth = 0;
+    let mut pos = 0;
+    let mut aim = 0;
+    include_str!("../../day2.txt")
+        .lines()
+        .filter(|l| ! l.is_empty())
+        .map(|l| l.split(" ").collect())
+        .map(|p: Vec<&str>| (p[0], p[1].parse::<i32>().unwrap()))
+        .for_each(|(c, x)| {             
+            match c {
+                "forward" => { 
+                    pos += x;
+                    depth += x * aim;
+                },
+                "up" => aim -= x,
+                "down" => aim += x,
+                _ => {}
+            }
+        });
+    println!(
+        "{}", depth*pos
+    );
+}
