a8e373453fda8180eca4671729ee0b3a2cedb361
Eric Broersma <git@ericbroersma.nl>
2021-12-03 15:49:49 +0100

    Day 3a

---
+++ day3a/src/main.rs
@@ -0,0 +1,27 @@
+pub fn main() {
+    let nums = include_str!("../../day3.txt")
+        .lines()
+        .map(|s| u32::from_str_radix(s, 2).unwrap());
+
+    let mut gamma: u32 = 0;
+
+    for i in 0..12 {
+        let mut total = 0;
+        
+        nums.clone().for_each(|n| {
+            if n & (1 << i) == 0 {
+                total -= 1;
+            } else {
+                total += 1;
+            }
+        });
+
+        if total > 0 {
+            gamma += 1 << i;
+        }
+    }
+
+    let epsilon = (!gamma) & 0b111111111111u32;
+
+    println!("{}", gamma * epsilon);
+}
