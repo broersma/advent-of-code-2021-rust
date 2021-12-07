ce583f3e7c6d099d76cc0ef22db9197df5c0ae62
Eric Broersma <git@ericbroersma.nl>
2021-12-07 15:48:43 +0100

    Just return a Vec instead of a Boxed Iterator and run cargo fmt

--- day5a/src/main.rs
+++ day5a/src/main.rs
@@ -1,8 +1,8 @@
+use itertools::EitherOrBoth::{Both, Left, Right};
+use itertools::Itertools;
+use std::collections::HashMap;
 use std::num::ParseIntError;
 use std::str::FromStr;
-use std::collections::HashMap;
-use itertools::Itertools;
-use itertools::EitherOrBoth::{Both, Left, Right};
 
 #[derive(Debug)]
 struct Line(u32, u32, u32, u32);
@@ -10,39 +10,44 @@ struct Line(u32, u32, u32, u32);
 impl Line {
     pub fn new(numbers: Vec<u32>) -> Line {
         assert_eq!(numbers.len(), 4);
-        
+
         Line(numbers[0], numbers[1], numbers[2], numbers[3])
     }
 
-    fn inclusive_range(a: u32, b: u32) -> Box<dyn Iterator<Item=u32>> {
+    fn inclusive_range(a: u32, b: u32) -> Vec<u32> {
         if a <= b {
-            Box::new(a..=b)
+            (a..=b).collect()
         } else {
-            Line::inclusive_range(b, a)
+            (b..=a).rev().collect()
         }
     }
 
     pub fn get_points(&self) -> Vec<(u32, u32)> {
-        Line::inclusive_range(self.0, self.2).zip_longest(Line::inclusive_range(self.1, self.3)).map(|e| match e {
-            Both(x,y) => (x,y),
-            Left(x) => (x,self.1),
-            Right(y) => (self.0, y)
-        }).collect()
+        let xs = Line::inclusive_range(self.0, self.2).into_iter();
+        let ys = Line::inclusive_range(self.1, self.3).into_iter();
+
+        xs.zip_longest(ys)
+            .map(|e| match e {
+                Both(x, y) => (x, y),
+                Left(x) => (x, self.1),
+                Right(y) => (self.0, y),
+            })
+            .collect()
     }
 }
 
 impl FromStr for Line {
     type Err = ParseIntError;
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        let numbers = s.split(" -> ")
-                       .flat_map(|x| x.split(","))
-                       .map(|n| n.parse::<u32>().unwrap())
-                       .collect();
+        let numbers = s
+            .split(" -> ")
+            .flat_map(|x| x.split(","))
+            .map(|n| n.parse::<u32>().unwrap())
+            .collect();
         let b = Line::new(numbers);
         Ok(b)
     }
 }
-
 
 pub fn main() {
     let input: Vec<_> = include_str!("../../day5.txt")
