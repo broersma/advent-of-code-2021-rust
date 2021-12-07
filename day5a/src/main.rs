e6ee0d597d8c2e862c7628b4318434ec038ab900
Eric Broersma <git@ericbroersma.nl>
2021-12-07 15:33:57 +0100

    Day 5a

---
+++ day5a/src/main.rs
@@ -0,0 +1,63 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+use std::collections::HashMap;
+use itertools::Itertools;
+use itertools::EitherOrBoth::{Both, Left, Right};
+
+#[derive(Debug)]
+struct Line(u32, u32, u32, u32);
+
+impl Line {
+    pub fn new(numbers: Vec<u32>) -> Line {
+        assert_eq!(numbers.len(), 4);
+        
+        Line(numbers[0], numbers[1], numbers[2], numbers[3])
+    }
+
+    fn inclusive_range(a: u32, b: u32) -> Box<dyn Iterator<Item=u32>> {
+        if a <= b {
+            Box::new(a..=b)
+        } else {
+            Line::inclusive_range(b, a)
+        }
+    }
+
+    pub fn get_points(&self) -> Vec<(u32, u32)> {
+        Line::inclusive_range(self.0, self.2).zip_longest(Line::inclusive_range(self.1, self.3)).map(|e| match e {
+            Both(x,y) => (x,y),
+            Left(x) => (x,self.1),
+            Right(y) => (self.0, y)
+        }).collect()
+    }
+}
+
+impl FromStr for Line {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let numbers = s.split(" -> ")
+                       .flat_map(|x| x.split(","))
+                       .map(|n| n.parse::<u32>().unwrap())
+                       .collect();
+        let b = Line::new(numbers);
+        Ok(b)
+    }
+}
+
+
+pub fn main() {
+    let input: Vec<_> = include_str!("../../day5.txt")
+        .lines()
+        .map(|n| n.parse::<Line>().unwrap())
+        .collect();
+
+    let mut map = HashMap::new();
+    for line in input {
+        if line.0 == line.2 || line.1 == line.3 {
+            for point in line.get_points() {
+                *map.entry(point).or_insert(0) += 1
+            }
+        }
+    }
+
+    println!("{}", map.values().filter(|&&x| x > 1).count());
+}
