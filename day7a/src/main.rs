5298aa0c40013a7b1660f127dfd905e80d4f984f
Eric Broersma <git@ericbroersma.nl>
2021-12-07 20:54:04 +0100

    Day 7a

---
+++ day7a/src/main.rs
@@ -0,0 +1,19 @@
+pub fn main() {
+    let input: Vec<_> = include_str!("../../day7.txt")
+        .lines()
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+
+    let min: i32 = *input.iter().min().unwrap();
+    let max: i32 = *input.iter().max().unwrap();
+
+    let mut fuel_usage = std::i32::MAX;
+    for i in min..=max {
+        fuel_usage = fuel_usage.min(input.iter().map(|n| (n - i).abs()).sum());
+    }
+
+    println!("{}", fuel_usage);
+}
